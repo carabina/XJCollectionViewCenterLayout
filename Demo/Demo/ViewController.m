b721ce194a2e58c3c87fcf6bf4de7732723fadc2
XJIMI <fn5128@gmail.com>
2017-10-12 19:02:13 +0800

    Demo Completed

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -7,23 +7,53 @@
 //
 
 #import "ViewController.h"
+#import "XJCollectionViewCenterLayout.h"
 
-@interface ViewController ()
+NSString *const CellIdentifier = @"CellIdentifier";
+
+@interface ViewController () < UICollectionViewDataSource,
+                               UICollectionViewDelegate >
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setupCollectionView];
 }
 
+- (void)setupCollectionView
+{
+    CGFloat inset = 20;
+    CGFloat size = self.view.frame.size.width - inset * 2;
+    XJCollectionViewCenterLayout *layout = [[XJCollectionViewCenterLayout alloc] init];
+    layout.minimumLineSpacing = 10;
+    layout.itemSize = CGSizeMake(size, size);
+    layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.collectionView.collectionViewLayout = layout;
+    self.collectionView.contentInset = UIEdgeInsetsMake(0, inset, 0, inset);
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    self.collectionView.decelerationRate = UIScrollViewDecelerationRateFast;
+
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:CellIdentifier];
 }
 
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor darkGrayColor];
+    return cell;
+}
 
 @end
